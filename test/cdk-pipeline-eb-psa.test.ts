6ba6ca88d3c1498f06207b13b6699b609cdb4cee
Jfisher37 <joseph.eric.fisher@gmail.com>
2023-09-04 10:55:47 -0400

    Initial commit

---
+++ test/cdk-pipeline-eb-psa.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipelineEbPsa from '../lib/cdk-pipeline-eb-psa-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-eb-psa-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipelineEbPsa.CdkPipelineEbPsaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
